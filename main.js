bdd68e71b4b351fe63e99292b44223f91c8134ea
Liu Jeffrey <jeffrey.liu@nokia-sbell.com>
2019-01-18 15:38:37 +0800

    implement load url text file and show image

--- main.js
+++ main.js
@@ -1,16 +1,45 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+
+const { app, BrowserWindow, Menu, dialog } = require('electron')
+const fs = require('fs');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({ width: 800, height: 600 })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
+  
+  // mainWindow.webContents.session.setProxy({proxyRules:"http://87.254.212.120:8080"}, function () {});
+  
+  Menu.setApplicationMenu(Menu.buildFromTemplate([
+    {
+      label: 'Menu',
+      submenu: [
+        {
+          label: 'Open URL File...',
+          accelerator: 'CmdOrCtrl+O',
+          click() {
+            dialog.showOpenDialog({ properties: ['openFile'] }, files => {
+              if (files && files.length) {
+                app.emit('open-url-file', files[0]);
+              }
+            })
+          }
+        },
+        { type: 'separator' },
+        {
+          label: 'Exit',
+          click() {
+            app.quit();
+          }
+        }]
+    }
+  ]));
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -48,3 +77,54 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+function* lineReader(filename) {
+  let fileSize = fs.statSync(filename).size;
+  let fd = fs.openSync(filename, 'r')
+  let bufferSize = 64 * 1024;
+  let readChunk = Buffer.alloc(bufferSize);
+  let position = 0;
+  let left = 0;
+  try {
+    while (position < fileSize) {
+      position += fs.readSync(fd, readChunk, left, bufferSize, position);
+      lines = readChunk.toString().split('\n');
+      for (let i = 0; i < lines.length - 1; ++i) {
+        yield lines[i];
+      }
+      left = lines[lines.length - 1].length;
+      readChunk.write(lines[lines.length - 1]);
+    }
+
+    if (left > 0) {
+      yield readChunk.slice(0, left).toString()
+    }
+  } catch (e) {
+    console.log('exception caught')
+    fs.closeSync(fd);
+    return;
+  }
+
+  fs.close(fd)
+}
+
+var urlFile;
+
+app.on('open-url-file', f => {
+  if (urlFile) {
+    urlFile.throw('close');
+  }
+
+  urlFile = lineReader(f);
+  app.emit('url-file-opened', f);
+});
+
+app.on('load-urls', arg => {
+  let {size = 10} = arg;
+
+  if (urlFile) {
+    for (let i = 0; i < size; ++i) {
+      app.emit('url-added', urlFile.next().value);
+    }
+  }
+})
--- renderer.js
+++ renderer.js
@@ -1,3 +1,15 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const{ remote } = require('electron')
+
+remote.app.on('url-file-opened', f => {
+    remote.app.emit('load-urls', {size: 15})
+})
+
+remote.app.on('url-added', url => {
+    let c = document.createElement('div');
+    c.innerHTML = "<img src=\"" + url + "\" ></img>";
+    document.querySelector('.gallery').appendChild(c);
+})
